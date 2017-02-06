4a234edebb6b64a741dacf3e2cea2db4c5a90307
chrisofarabia <chrisofarabia@yahoo.co.uk>
2017-02-06 09:47:20 +0000

    adding MONGOLAB to config.db

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: process.env.MONGO_URI || 'mongodb://localhost/photo-walks',
+  db: process.env.MONGOLAB_URI || 'mongodb://localhost/photo-walks',
   secret: process.env.SECRET || 'photo walks are such fun'
 };
